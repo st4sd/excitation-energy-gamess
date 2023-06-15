a2728af39061fa709a3bb15ceed2c412faa940ec
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2023-06-15 14:08:47 +0100

    fest: add support for the CPU-optimized GAMESS US image
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- deploy/etest-semi-empirical.sh
+++ deploy/etest-semi-empirical.sh
@@ -4,9 +4,13 @@ dirScripts=`dirname "${0}"`
 
 cd ${dirScripts}/../
 
-
 export TEST_IMAGE=${TEST_IMAGE:-quay.io/st4sd/official-base/st4sd-runtime-core:platform-release-latest}
 docker pull ${TEST_IMAGE}
-docker run --rm -it -v "`pwd`:/package/" -w /tmp --entrypoint sh ${TEST_IMAGE} -c \
-  "etest.py --manifest=/package/semi-empirical/manifest.yaml -l20 --platform=openshift \
-  --notestExecutables /package/semi-empirical/excitation-energy-se-gamess-us.yaml"
+
+for platform in docker docker-cpu openshift openshift-kubeflux openshift-cpu
+do
+  echo "### Testing ${platform}"
+  docker run --rm -it -v "`pwd`:/package/" -w /tmp --entrypoint sh ${TEST_IMAGE} -c \
+    "etest.py --manifest=/package/semi-empirical/manifest.yaml -l40 --platform=${platform} \
+    --notestExecutables /package/semi-empirical/excitation-energy-se-gamess-us.yaml"
+done
